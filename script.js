b9b1def50525a880338a6eef8aa0aab09cd0c182
0lir055 <olirossxiv@gmail.com>
2024-01-11 19:07:18 +0000

    Added a question wrapper to the game.html file as well as adding options for the answers with the question number at the top

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const start = document.querySelector('#start'),
 
 let questions = [];
 
+let score = 0;
+
 const num = numquestion.value;
 const dif = difficulty.value;
 const cat = category.value;
@@ -13,10 +15,13 @@ const cat = category.value;
 
 fetch ( 'https://opentdb.com/api.php?amount=${num}&category=${cat}&difficulty=${dif}&type=multiple')
 
-.then (res => res.json())
-.then (data => {
-  questions = data.results;
-  console.log(questions);
-})
-.catch (err => console.log(err));
+  .then (res => {
+    return res.json();
+  })
+  .then (loadedQuestions => {
+    console.log(loadedQuestions);
+  } )
 
+  .catch (err => {
+    console.error(err);
+  } );
