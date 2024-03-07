abed6561cbc1092ea539322f579115277eb5b666
Oliver Ross <olirossxiv@gmail.com>
2024-03-07 08:46:48 +0000

    Fix stylesheet link in end-screen.html and remove script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const StartButton = document.queryselector('.start'),
-  numquestions = document.queryselector('#amount'),
-  category = document.queryselector('#category'),
-  difficulty = document.queryselector('#difficulty'),
-  timeperqeustion = document.queryselector('#time');
-
